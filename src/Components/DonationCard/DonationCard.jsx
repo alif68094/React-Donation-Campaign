104dfbbe8a084f58c0bfc7848f7f738c0cca24cc
Md. Alif <110423275+alif68094@users.noreply.github.com>
2023-09-25 06:07:17 +0600

    added see all functionality

--- src/Components/DonationCard/DonationCard.jsx
+++ src/Components/DonationCard/DonationCard.jsx
@@ -3,7 +3,8 @@
 const DonationCard = ({ item }) => {
     const { id, picture, title, category, category_bg_color, card_bg_color, text_color, button_bg_color, description, price } = item;
     return (
-        <div style={{ backgroundColor: `${card_bg_color}` }} className="flex gap-3 p-4 items-center rounded">
+       <div>
+         <div style={{ backgroundColor: `${card_bg_color}` }} className="flex gap-3 p-4 items-center rounded">
             <div>
                 <img src={picture} alt="" />
             </div>
@@ -16,7 +17,10 @@ const DonationCard = ({ item }) => {
                 <p style={{ color: `${text_color}` }} className="mb-2">${price}</p>
                 <button style={{ backgroundColor: `${text_color}` }} className="px-4 py-1 rounded text-white">View Details</button>
             </div>
+            
         </div>
+        
+       </div>
     );
 };
 
--- src/Pages/Donation.jsx
+++ src/Pages/Donation.jsx
@@ -5,34 +5,46 @@ import DonationCard from "../Components/DonationCard/DonationCard";
 const Donation = () => {
     const [donation, setDonation] = useState([]);
     const [noData, setNoData] = useState("")
-        useEffect(() => {
-            const donatedCategory = JSON.parse(localStorage.getItem('donations'))
+    const [isShow,setIsShow] = useState(false)
+    useEffect(() => {
+        const donatedCategory = JSON.parse(localStorage.getItem('donations'))
 
-            
-            if(donatedCategory){
-                setDonation(donatedCategory);
-            }
-            else{
-                setNoData("No Donation Found")
-            }
-            
-            
-        },[])
-        
+
+        if (donatedCategory) {
+            setDonation(donatedCategory);
+        }
+        else {
+            setNoData("No Donation Found")
+        }
+
+
+    }, [])
+
     return (
-        
+
         <div>
             {
-                noData? <p className="text-center mt-12">{noData}</p> : <div>
+                noData ? <p className="text-center mt-12">{noData}</p> : <div>
 
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
                         {
-                           donation.map(item => <DonationCard item={item} key={item.id}></DonationCard>) 
+                            isShow? donation.map(item => <DonationCard item={item} key={item.id}></DonationCard>) : 
+                            donation.slice(0,4).map(item => <DonationCard item={item} key={item.id}></DonationCard>)
                         }
                     </div>
 
                 </div>
             }
+            {
+                donation.length > 4 && <div className="flex justify-center">
+                <button
+                    onClick={()=>setIsShow(!isShow)}
+                    className="bg-[#009444] px-5 py-2 mt-6 text-center text-white rounded">
+                    {!isShow? "See All" : "Show Less"}</button>
+            </div>
+            }
+            
+            
         </div>
     );
 };
